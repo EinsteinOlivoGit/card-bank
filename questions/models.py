850d54228bfac6207eaee4544cd18ad66ec2e4c4
einsteinolivo <einsteinolivo@gmail.com>
2022-06-05 07:43:25 -0400

    Adding cards to migrations and changing questions models

--- questions/models.py
+++ questions/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Content(models.Model):
     content_text = models.CharField(max_length=2000)
+    topic = models.CharField(max_length=100)
     pub_date = models.DateTimeField('date published')
     state = models.CharField(max_length=2)
 
 class Question(models.Model):
     content = models.ForeignKey(Content, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=400)
-    topic = models.CharField(max_length=100)
     pub_date = models.DateTimeField('date published')
     state = models.CharField(max_length=2)
 
--- questions_bank/settings.py
+++ questions_bank/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cards.apps.CardsConfig',
     'questions.apps.QuestionsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
