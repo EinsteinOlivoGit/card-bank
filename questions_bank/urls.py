541645c003ce32b33a4a8ec8588a273ed65c6d91
einsteinolivo <einsteinolivo@gmail.com>
2022-10-23 22:34:17 -0400

    Dandole protagonismo a las tarjetas

--- questions_bank/urls.py
+++ questions_bank/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('cards/', include('cards.urls')),
-    path('', include('questions.urls')),
+    path('', include('cards.urls')),
+    path('questions/', include('questions.urls')),
     path('admin/', admin.site.urls),
 ]
