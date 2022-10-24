9a0fecb87581a31258b279b7534e2eeb7aef1626
einsteinolivo <einsteinolivo@gmail.com>
2022-10-24 03:16:24 -0400

    Renombrando Procfile

--- questions_bank/settings.py
+++ questions_bank/settings.py
@@ -22,7 +22,7 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6k++1p543evu=@w=yuv0!3ve977l*+m^_y%+)ye^3m*nm_atlo'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
